ae7316cd59c008b12fe6f53b17828d1d1a115dc9
Maycon Miranda <mayconjr_@live.com>
2017-11-27 22:12:06 -0200

    corrections

--- app/src/main/java/br/ufop/icea/encontrodesaberes/view/AboutActivity.java
+++ app/src/main/java/br/ufop/icea/encontrodesaberes/view/AboutActivity.java
@@ -3,6 +3,8 @@ package br.ufop.icea.encontrodesaberes.view;
 import android.app.Activity;
 import android.os.Bundle;
 
+import br.ufop.icea.encontrodesaberes.R;
+
 public class AboutActivity extends Activity {
 
     @Override
